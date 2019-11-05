47613e4239df31ffd8a1498219e16b3ee951909d
zungmann <zungmann@yahoo.com>
2019-11-05 07:35:19 +0700

    change model download link

--- app/server.py
+++ app/server.py
@@ -9,7 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=14n2pq1gmVmMePtOkiIwdiYMV-UPH77uS'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
@@ -30,9 +31,9 @@ async def download_file(url, dest):
 
 
 async def setup_learner():
-    await download_file(export_file_url, path / export_file_name)
+    await download_file(export_file_url, path / 'models' / export_file_name)
     try:
-        learn = load_learner(path, export_file_name)
+        learn = load_learner(path/'models', export_file_name)
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
